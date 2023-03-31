3fb8e926ea965e1e1e44dfe646404c36f0cd16ee
vit <vitaliykorshikov@yandex.ru>
2023-03-31 10:20:50 +0300

    pep

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,12 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (
-    IngredientsViewSets,
-    RecipeViewSets,
-    TagsViewSets,
-    UserViewSets
-)
+from .views import (IngredientsViewSets, RecipeViewSets, TagsViewSets,
+                    UserViewSets)
 
 router = DefaultRouter()
 
